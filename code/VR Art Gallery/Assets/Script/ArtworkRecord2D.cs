3d253145e1f510d1f7deef204e7b22db66d2d172
Diksha Chottani <dikshachottani@gmail.com>
2026-01-29 16:21:57 +0000

    Canvas Prefab

---
+++ code/VR Art Gallery/Assets/Script/ArtworkRecord2D.cs
@@ -0,0 +1,10 @@
+[Serializable]
+public class ArtworkRecord2D
+{
+    public string id;
+    public string title;
+    public string author;
+    public string description;
+    public int storage_path;
+    public string updated_at;
+}
