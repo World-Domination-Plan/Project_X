8ff2fe060900d625d779150494108023b8d1618e
batunii <sy.soni@outlook.com>
2026-04-01 16:51:36 +0100

    GallerySync Script and object added
    A GallerySync object has been added with the relevant script, it works when creating a new room, we need to test if it works when joining the same room as well, or not.

---
+++ code/VR Art Gallery/Assets/Scripts/Network/GallerySync.cs
@@ -0,0 +1,46 @@
+using Unity.Netcode;
+using Unity.Collections;
+using System.Diagnostics;
+using UnityEngine;
+
+public class GallerySync : NetworkBehaviour
+{
+    // Only the server can write; all clients can read
+    private NetworkVariable<int> galleryId = new NetworkVariable<int>(
+        0,
+        NetworkVariableReadPermission.Everyone,
+        NetworkVariableWritePermission.Server
+    );
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+
+        if (IsServer)
+        {
+            // Player B (host) sets the gallery ID when this object spawns
+            galleryId.Value = 1; // your logic here
+            UnityEngine.Debug.Log($"[GallerySync] Server set galleryId to {galleryId.Value}");
+        }
+        else
+        {
+            // Player A: value is already synced by this point, read it
+            LoadGallery(galleryId.Value);
+            UnityEngine.Debug.Log($"[GallerySync] Client read initial galleryId: {galleryId.Value}");
+        }
+
+        // Subscribe to future changes (in case host changes gallery later)
+        galleryId.OnValueChanged += OnGalleryIdChanged;
+    }
+
+    private void OnGalleryIdChanged(int previous, int current)
+    {
+        if (!IsServer)
+            LoadGallery(current);
+    }
+
+    private void LoadGallery(int id)
+    {
+        // Render your gallery using the id
+    }
+}
